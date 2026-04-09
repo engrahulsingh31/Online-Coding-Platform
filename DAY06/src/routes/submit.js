ebb8578769bfeef805c7c7cda9753c2a86ba0aab
Rahul Singh <rs3304481@gmail.com>
2026-04-09 09:14:30 +0530

    Deleted DAY06

--- DAY06/src/routes/submit.js
+++
@@ -1,8 +0,0 @@
-const express= require("express")
-const submitrouter=express.Router()
-const usermiddleware=require("../middleware/usermiddleware")
-
-
-submitrouter.post("/submit/:id",usermiddleware,submitcode)
-
-module.exports=submitrouter 
